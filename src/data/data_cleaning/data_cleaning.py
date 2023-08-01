5fbd656a35441f2d30d7cbcb4217e8355fed37c6
RomainPen <romain.pen.pro16@gmail.com>
2023-08-01 19:21:50 +0200

    run main.py

--- src/data/data_cleaning/data_cleaning.py
+++ src/data/data_cleaning/data_cleaning.py
@@ -40,6 +40,8 @@ class Data_Cleaning :
         #age first contract :
         df["age_first_contract"] = df["CUSTOMER_AGE"] - (df["CONTRACT_TENURE_DAYS"]/365)
         df.loc[df['age_first_contract'] < 18, 'CONTRACT_TENURE_DAYS'] = np.nan
+        df = df.drop(["age_first_contract"], axis=1)
+        
         # 'NO_OF_RECHARGES_6M' :
         df.loc[(df['NO_OF_RECHARGES_6M']-df['FAILED_RECHARGE_6M']) < 0, 'NO_OF_RECHARGES_6M'] = np.nan
         # proportion de valeurs négatives pour les trois variables concernées
@@ -71,34 +73,34 @@ class Data_Cleaning :
     def impute_missing_values(self, df : pd.DataFrame, target : str, train :bool) :
         
         if train :
-            df_train = df.copy()
+            self.df_train = df.copy()
             #Treat df_train :
             #1.1/ Drop rows and col which are more 60-70% of NaN.
             #If there is 50%-60% or more NaN, drop the column.
-            self.df_train_na_col = (pd.DataFrame(((df_train.isna().sum(axis=0))/len(df_train))*100)).rename(columns={0: 'Sum_NaN_%'})
+            self.df_train_na_col = (pd.DataFrame(((self.df_train.isna().sum(axis=0))/len(self.df_train))*100)).rename(columns={0: 'Sum_NaN_%'})
             #drop col :
-            df_train = df_train.drop(columns=list((self.df_train_na_col[self.df_train_na_col["Sum_NaN_%"]>50]).index))
+            self.df_train = self.df_train.drop(columns=list((self.df_train_na_col[self.df_train_na_col["Sum_NaN_%"]>50]).index))
             #Count the number of NaN in rows :
             #If there is 60-70% or more NaN, drop the row.
-            df_train_na_row = (pd.DataFrame(((df_train.isna().sum(axis=1))/len(df_train.columns))*100)).rename(columns={0: 'Sum_NaN_%'})
+            df_train_na_row = (pd.DataFrame(((self.df_train.isna().sum(axis=1))/len(self.df_train.columns))*100)).rename(columns={0: 'Sum_NaN_%'})
             #drop row :
-            df_train = df_train.drop(index=list((df_train_na_row[df_train_na_row["Sum_NaN_%"]>60]).index))
+            self.df_train = self.df_train.drop(index=list((df_train_na_row[df_train_na_row["Sum_NaN_%"]>60]).index))
             #reset_index :
-            df_train = df_train.reset_index(drop = True)
+            self.df_train = self.df_train.reset_index(drop = True)
             
             #1.2/ dectect col which have NaN
-            df_train_na = (pd.DataFrame(df_train.isna().sum())).rename(columns={0: 'Sum_NaN'})
+            df_train_na = (pd.DataFrame(self.df_train.isna().sum())).rename(columns={0: 'Sum_NaN'})
             df_train_na_egal_0 = df_train_na[df_train_na['Sum_NaN']==0]
             df_train_na_diff_0 = df_train_na[df_train_na['Sum_NaN']!=0]
             
             #*********************************review this part************************************
             #1.3/Build groupby df for fillna : 
             #select best col for groupby fillna :
-            corr_matrix = df_train[list(df_train_na_egal_0.index)].corr().abs().loc[target].drop([target], axis=0)
-            corr_matrix = pd.concat([corr_matrix, df_train[list(corr_matrix.index)].nunique()], axis=1).rename(columns={0: 'nunique'})
-            self.df_train_grouby_3var = df_train.groupby(["PASS_GRACE_IND_M1", "PASS_GRACE_IND_M2", "PASS_GRACE_IND_M3"])
-            self.df_train_grouby_2var = df_train.groupby(["PASS_GRACE_IND_M1", "PASS_GRACE_IND_M2"])
-            self.df_train_grouby_1var = df_train.groupby(["PASS_GRACE_IND_M1"])
+            corr_matrix = self.df_train[list(df_train_na_egal_0.index)].corr().abs().loc[target].drop([target], axis=0)
+            corr_matrix = pd.concat([corr_matrix, self.df_train[list(corr_matrix.index)].nunique()], axis=1).rename(columns={0: 'nunique'})
+            self.df_train_grouby_3var = self.df_train.groupby(["PASS_GRACE_IND_M1", "PASS_GRACE_IND_M2", "PASS_GRACE_IND_M3"])
+            self.df_train_grouby_2var = self.df_train.groupby(["PASS_GRACE_IND_M1", "PASS_GRACE_IND_M2"])
+            self.df_train_grouby_1var = self.df_train.groupby(["PASS_GRACE_IND_M1"])
             #*************************************************************************************
             
             #1.4/fillna on dataset : 
@@ -108,11 +110,11 @@ class Data_Cleaning :
             #df_train[list_cont_particular_imputation] = df_train[list_cont_particular_imputation].fillna(common value, example = 0)
             #fillna with median :
             for i in (df_train_na_diff_0.index) : # or replace (df_train_na_diff_0.index) by list_median_mode_imputation
-                if is_numeric_dtype(df_train[i]) and i != target :
-                    df_train[i] = self.df_train_grouby_3var[i].fillna(df_train[i].median()) 
-                    df_train[i] = self.df_train_grouby_2var[i].fillna(df_train[i].median())
-                    df_train[i] = self.df_train_grouby_1var[i].fillna(df_train[i].median())
-                    df_train[i] = df_train[i].fillna(df_train[i].median())
+                if is_numeric_dtype(self.df_train[i]) and i != target :
+                    self.df_train[i] = self.df_train_grouby_3var[i].fillna(self.df_train[i].median()) 
+                    self.df_train[i] = self.df_train_grouby_2var[i].fillna(self.df_train[i].median())
+                    self.df_train[i] = self.df_train_grouby_1var[i].fillna(self.df_train[i].median())
+                    self.df_train[i] = self.df_train[i].fillna(self.df_train[i].median())
 
             # categorical col :
             #fillna with particular value :
@@ -120,26 +122,26 @@ class Data_Cleaning :
             #df_train[list_cat_particular_imputation].fillna("cat_value", inplace=True)
             #***************************************to review*******************************************
             # try to find a manipulation with setting.yaml
-            df_train['marque'] = df_train['marque'].fillna("unknown")
+            self.df_train['marque'] = self.df_train['marque'].fillna("unknown")
             #*******************************************************************************************
             #fillna with mode :
             for i in (df_train_na_diff_0.index) :  # or replace (df_train_na_diff_0.index) by list_median_mode_imputation
-                if is_object_dtype(df_train[i]) and i != target :
-                    df_train[i] = self.df_train_grouby_3var[i].fillna(df_train[i].mode()[0]) ##df_train.groupby(["PASS_GRACE_IND_M1", "PASS_GRACE_IND_M2", "PASS_AFTERGRACE_IND_M2"])[i].fillna(df_train[i].mode())
-                    df_train[i] = self.df_train_grouby_2var[i].fillna(df_train[i].mode()[0])
-                    df_train[i] = self.df_train_grouby_1var[i].fillna(df_train[i].mode()[0])
-                    df_train[i] = df_train[i].fillna(df_train[i].mode()[0])
+                if is_object_dtype(self.df_train[i]) and i != target :
+                    self.df_train[i] = self.df_train_grouby_3var[i].fillna(self.df_train[i].mode()[0]) ##df_train.groupby(["PASS_GRACE_IND_M1", "PASS_GRACE_IND_M2", "PASS_AFTERGRACE_IND_M2"])[i].fillna(df_train[i].mode())
+                    self.df_train[i] = self.df_train_grouby_2var[i].fillna(self.df_train[i].mode()[0])
+                    self.df_train[i] = self.df_train_grouby_1var[i].fillna(self.df_train[i].mode()[0])
+                    self.df_train[i] = self.df_train[i].fillna(self.df_train[i].mode()[0])
             
-            return df_train
+            return self.df_train
           
                 
         else :
-            df_val = df.copy()
+            self.df_val = df.copy()
             # Treat df_val :
             #1.1/ Drop rows and col which are more 60-70% of NaN.
             #If there is 50%-60% or more NaN, drop the column.
             #drop col :
-            df_val.drop(columns=list((self.df_train_na_col[self.df_train_na_col["Sum_NaN_%"]>50]).index), inplace=True)
+            self.df_val.drop(columns=list((self.df_train_na_col[self.df_train_na_col["Sum_NaN_%"]>50]).index), inplace=True)
             #Count the number of NaN in rows :
             #If there is 60-70% or more NaN, drop the row.
             #useless for row of df_val 
@@ -152,13 +154,13 @@ class Data_Cleaning :
             # if there is NaN, fillna them by the median/mean or mode from df_train
             #numerical col :
             for i in groupby_columns :
-                df_val[i] = df_val[i].fillna(df_train[i].median())
+                self.df_val[i] = self.df_val[i].fillna(self.df_train[i].median())
             #cat col :
             for i in groupby_columns :
-                df_val[i] = df_val[i].fillna(df_train[i].mode())
+                self.df_val[i] = self.df_val[i].fillna(self.df_train[i].mode())
                 
             #1.2/dectect col which have NaN
-            df_val_na = (pd.DataFrame(df_val.isna().sum())).rename(columns={0: 'Sum_NaN'})
+            df_val_na = (pd.DataFrame(self.df_val.isna().sum())).rename(columns={0: 'Sum_NaN'})
             #df_val_na_egal_0 = df_val_na[df_val_na['Sum_NaN']==0]
             df_val_na_diff_0 = df_val_na[df_val_na['Sum_NaN']!=0]
             
@@ -172,63 +174,63 @@ class Data_Cleaning :
             #fillna with median : 
             def imputate_missing_3val_num(df) :
                 if pd.isna(df[i]) :
-                    return (self.df_train_grouby_3var.get_group(("PASS_GRACE_IND_M1"==df["PASS_GRACE_IND_M1"], "PASS_GRACE_IND_M2"==df["PASS_GRACE_IND_M2"], "PASS_GRACE_IND_M3"==df["PASS_GRACE_IND_M3"]))[[elem for elem in df_val.columns if is_numeric_dtype(df_val[elem])]].agg("median"))[i]
+                    return (self.df_train_grouby_3var.get_group(("PASS_GRACE_IND_M1"==df["PASS_GRACE_IND_M1"], "PASS_GRACE_IND_M2"==df["PASS_GRACE_IND_M2"], "PASS_GRACE_IND_M3"==df["PASS_GRACE_IND_M3"]))[[elem for elem in self.df_val.columns if is_numeric_dtype(self.df_val[elem])]].agg("median"))[i]
                 else :
                     return df[i]
 
             def imputate_missing_2val_num(df) :
                 if pd.isna(df[i]) :
-                    return (self.df_train_grouby_2var.get_group(("PASS_GRACE_IND_M1"==df["PASS_GRACE_IND_M1"], "PASS_GRACE_IND_M2"==df["PASS_GRACE_IND_M2"]))[[elem for elem in df_val.columns if is_numeric_dtype(df_val[elem])]].agg("median"))[i]
+                    return (self.df_train_grouby_2var.get_group(("PASS_GRACE_IND_M1"==df["PASS_GRACE_IND_M1"], "PASS_GRACE_IND_M2"==df["PASS_GRACE_IND_M2"]))[[elem for elem in self.df_val.columns if is_numeric_dtype(self.df_val[elem])]].agg("median"))[i]
                 else :
                     return df[i]
 
             def imputate_missing_1val_num(df) :
                 if pd.isna(df[i]) :
-                    return (self.df_train_grouby_1var.get_group(("PASS_GRACE_IND_M1"==df["PASS_GRACE_IND_M1"]))[[elem for elem in df_val.columns if is_numeric_dtype(df_val[elem])]].agg("median"))[i]
+                    return (self.df_train_grouby_1var.get_group(("PASS_GRACE_IND_M1"==df["PASS_GRACE_IND_M1"]))[[elem for elem in self.df_val.columns if is_numeric_dtype(self.df_val[elem])]].agg("median"))[i]
                 else :
                     return df[i]
             
             for i in (df_val_na_diff_0.index) : #or list_median_mode_imputation
-                if is_numeric_dtype(df_val[i]) and i != target :
-                    df_val[i] = df_val.apply(imputate_missing_3val_num, axis=1)
-                    df_val[i] = df_val.apply(imputate_missing_2val_num, axis=1)
-                    df_val[i] = df_val.apply(imputate_missing_1val_num, axis=1)
-                    df_val[i] = df_val[i].fillna(df_train[i].median())
+                if is_numeric_dtype(self.df_val[i]) and i != target :
+                    self.df_val[i] = self.df_val.apply(imputate_missing_3val_num, axis=1)
+                    self.df_val[i] = self.df_val.apply(imputate_missing_2val_num, axis=1)
+                    self.df_val[i] = self.df_val.apply(imputate_missing_1val_num, axis=1)
+                    self.df_val[i] = self.df_val[i].fillna(self.df_train[i].median())
                     
             # caterical col :
             #fillna with particular value :
             #df_val["var"] = df_val["var"].fillna("categorical_value")
-            df_val['marque'].fillna("unknown", inplace=True)
+            self.df_val['marque'].fillna("unknown", inplace=True)
             #fillna with mode :
             def imputate_missing_3val_cat(df) :
                 if pd.isna(df[i]) :
                     return (self.df_train_grouby_3var.get_group(("PASS_GRACE_IND_M1"==df["PASS_GRACE_IND_M1"], "PASS_GRACE_IND_M2"==df["PASS_GRACE_IND_M2"],
-                                                            "PASS_GRACE_IND_M3"==df["PASS_GRACE_IND_M3"]))[[elem for elem in df_val.columns if is_object_dtype(df_val[elem])]].agg("mode"))[i][0]
+                                                            "PASS_GRACE_IND_M3"==df["PASS_GRACE_IND_M3"]))[[elem for elem in self.df_val.columns if is_object_dtype(self.df_val[elem])]].agg("mode"))[i][0]
                 else :
                     return df[i]
                 
             def imputate_missing_2val_cat(df) :
                 if pd.isna(df[i]) :
                     return (self.df_train_grouby_2var.get_group(("PASS_GRACE_IND_M1"==df["PASS_GRACE_IND_M1"],
-                                                            "PASS_GRACE_IND_M2"==df["PASS_GRACE_IND_M2"]))[[elem for elem in df_val.columns if is_object_dtype(df_val[elem])]].agg("mode"))[i][0]
+                                                            "PASS_GRACE_IND_M2"==df["PASS_GRACE_IND_M2"]))[[elem for elem in self.df_val.columns if is_object_dtype(self.df_val[elem])]].agg("mode"))[i][0]
                 else :
                     return df[i]
 
             def imputate_missing_1val_cat(df) :
                 if pd.isna(df[i]) :
-                    return (self.df_train_grouby_1var.get_group(("PASS_GRACE_IND_M1"==df["PASS_GRACE_IND_M1"]))[[elem for elem in df_val.columns if is_object_dtype(df_val[elem])]].agg("mode"))[i][0]
+                    return (self.df_train_grouby_1var.get_group(("PASS_GRACE_IND_M1"==df["PASS_GRACE_IND_M1"]))[[elem for elem in self.df_val.columns if is_object_dtype(self.df_val[elem])]].agg("mode"))[i][0]
                 else :
                     return df[i]
 
             for i in (df_val_na_diff_0.index) : #or list_median_mode_imputation
-                if is_object_dtype(df_val[i]) and i != target :
-                    if df_val[i].isnull().values.any() :
-                        df_val[i] = df_val.apply(imputate_missing_3val_cat, axis=1)
-                        df_val[i] = df_val.apply(imputate_missing_2val_cat, axis=1)
-                        df_val[i] = df_val.apply(imputate_missing_1val_cat, axis=1)
-                        df_val[i] = df_val[i].fillna(df_train[i].mode())
+                if is_object_dtype(self.df_val[i]) and i != target :
+                    if self.df_val[i].isnull().values.any() :
+                        self.df_val[i] = self.df_val.apply(imputate_missing_3val_cat, axis=1)
+                        self.df_val[i] = self.df_val.apply(imputate_missing_2val_cat, axis=1)
+                        self.df_val[i] = self.df_val.apply(imputate_missing_1val_cat, axis=1)
+                        self.df_val[i] = self.df_val[i].fillna(self.df_train[i].mode())
             
-            return df_val
+            return self.df_val
             #******************************************************************************************************
         
         
--- src/visualization/EDA.py
+++ src/visualization/EDA.py
@@ -6,7 +6,7 @@ class EDA:
         pass
     
     def general_summary_skimpy(self, df):
-        skim(df)
+        #skim(df)
         return None
     
     def target_feature_distribution_groupby_categorical_features(self, df):
